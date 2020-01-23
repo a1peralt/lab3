1764a5ee61d5e58cef6d4257696b938330379ce5
Albert Peralta <a1peralt@ucsd.edu>
2020-01-22 21:18:48 -0800

    JS added

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,30 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
+
 		$('.jumbotron h1').text("Javascript is connected");
+
+		$('.jumbotron p').toggleClass("active");
+
+		$("#testjs").text("JavaScript Loaded!");
 	});
+
+	$(".project").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+
+function projectClick(e) {
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.toggle();
+    }
+
+    e.preventDefault();
 }
